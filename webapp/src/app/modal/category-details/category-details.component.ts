3880bf09d064da1ca3b83805d2e6bc3b63e51652
vfon98 <vfon98@gmail.com>
2020-07-29 08:01:35 +0700

    Basic category

--- webapp/src/app/modal/category-details/category-details.component.ts
+++ webapp/src/app/modal/category-details/category-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ICategory} from "../../manager/category-management/category.service";
+import {CategoryModalService} from "../../service/category-modal.service";
 
 @Component({
   selector: 'app-category-details',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-details.component.css']
 })
 export class CategoryDetailsComponent implements OnInit {
+  category: ICategory;
 
-  constructor() { }
+  constructor(private categoryModalService: CategoryModalService) { }
 
   ngOnInit(): void {
+  }
+
+  hideModal(): void {
+    this.categoryModalService.hideDetailsModal();
   }
 
 }
--- webapp/src/app/modal/category-update/category-update.component.ts
+++ webapp/src/app/modal/category-update/category-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {CategoryModalService} from "../../service/category-modal.service";
+import {CategoryService, ICategory} from "../../manager/category-management/category.service";
+import {NotificationService} from "../../layouts/notification/notification.service";
 
 @Component({
   selector: 'app-category-update',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-update.component.css']
 })
 export class CategoryUpdateComponent implements OnInit {
+  category: ICategory;
 
-  constructor() { }
+  constructor(
+    private categoryModalService: CategoryModalService,
+    private categoryService: CategoryService,
+    private notiService: NotificationService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  hideModal(): void {
+    this.categoryModalService.hideUpdateModal();
+  }
+
+  handleSubmit(id: number, body: ICategory): void {
+    this.categoryService.update(id, body).subscribe(category => {
+      this.notiService.showSuccess();
+      this.hideModal();
+    })
+  }
 }
